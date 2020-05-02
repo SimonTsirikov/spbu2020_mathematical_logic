c8829e0106f95ca82f18745c1be79a37e3ea73e7
gleb.mirzazyanov@gmail.com <gleb.mirzazyanov@gmail.com>
2020-05-02 18:47:09 +0300

    fixed ast construct and trying write new parser

--- ast.py
+++ ast.py
@@ -1,64 +1,58 @@
 from rply.token import BaseBox
 
 
+class Var(BaseBox):
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.name == other.name
+
+    def copy(self):
+        return self.__class__(self.name)
+
+    def show(self):
+        return f'{self.__class__.__name__} {self.name}'
+
+
 class Term(BaseBox):
     def __init__(self, name, args=None):
         self.name = name
         self.args = args
-        if args is not None:
-            for item in args:
-                if not isinstance(item, Term):
-                    raise ValueError(f'Inappropriate argument for "{self.name}": {item.show()} should be Term, not {item.__class__.__name__}.')
-    
-    def __eq__(self, other): 
-        if self.__class__ == other.__class__ and self.name == other.name:
-            eq_args = self.args is None and other.args is None 
-            if not (self.args is None or other.args is None):
-                eq_args = (len(self.args) == len(other.args) and all([i == j for i, j in zip(self.args, other.args)]))     
-            return eq_args
-        return False
 
-    def copy(self):
-        if self.args is None:
-            return self.__class__(self.name)
-        else:
-            return self.__class__(self.name, self.args.copy())
-
-    def show(self):
-        if self.args is None:
-            return f'{self.__class__.__name__} {self.name}'
-        else:
-            return f'{self.__class__.__name__} {self.name}({", ".join(map(lambda x: x.show(), self.args))})'
-
-
-class Atom(BaseBox):
-    def __init__(self, name, args):
-        self.name = name
-        self.args = args
-        for item in args:
-            if not isinstance(item, Term):
-                raise ValueError(f'Inappropriate argument for "{self.name}": {item.show()} should be Term, not {item.__class__.__name__}.')
-    
     def __eq__(self, other):
-        if self.__class__ == other.__class__ and self.name == other.name:
-            return (len(self.args) == len(other.args) and all([i == j for i, j in zip(self.args, other.args)]))     
-        return False
+        eq_type = self.__class__ == other.__class__ and self.name == other.name
+        if eq_type:
+            eq_args = self.args is None and other.args is None
+            if not eq_args and not (self.args is None or other.args is None):
+                eq_args = (len(self.args) == len(other.args) and all([i == j for i, j in zip(self.args, other.args)]))
+        return eq_type and eq_args
 
     def copy(self):
-        return self.__class__(self.name, self.args.copy())
+        return self.__class__(self.name, self.args)
 
     def show(self):
         if self.args is None:
             return f'{self.__class__.__name__} {self.name}'
         else:
             return f'{self.__class__.__name__} {self.name}({", ".join(map(lambda x: x.show(), self.args))})'
+
+
+class Atom(Term):
+    def __init__(self, name, args):
+        super().__init__(name, args)
+
+    def __eq__(self, other):
+        eq_type = self.__class__ == other.__class__ and self.name == other.name
+        if eq_type:
+            eq_args = (len(self.args) == len(other.args) and all([i == j for i, j in zip(self.args, other.args)]))
+        return eq_type and eq_args
+
 
 class UnaryOp:
     def __init__(self, argument):
-        if not isinstance(argument, Term):
-            self.argument = argument
-        else:
-            raise ValueError(f'In {self.__class__.__name__}, {argument.show()} should be Expr, not {argument.__class__.__name__}.')
+        self.argument = argument
+
     def __eq__(self, other):
         return self.__class__ == other.__class__ and self.argument == other.argument
 
@@ -71,14 +65,8 @@ class UnaryOp:
 
 class BinaryOp(BaseBox):
     def __init__(self, left, right):
-        if not (isinstance(left, Term) or isinstance(right, Term)):
-            self.left = left
-            self.right = right
-        else:
-            if isinstance(left, Term):
-                raise ValueError(f'In {self.__class__.__name__}, {left.show()} should be Expr, not {left.__class__.__name__}.')
-            if isinstance(right, Term):
-                raise ValueError(f'In {self.__class__.__name__}, {right.show()} should be Expr, not {left.__class__.__name__}.')
+        self.left = left
+        self.right = right
 
     def __eq__(self, other):
         return self.__class__ == other.__class__ and self.left == other.left and self.right == other.right
@@ -90,15 +78,19 @@ class BinaryOp(BaseBox):
         return f'{self.__class__.__name__}({self.left.show()},{self.right.show()})'
 
 
-class Negation(UnaryOp):
+class Neg(UnaryOp):
     def introduce(self):
         return [(self, [], [self.argument])]
 
     def eliminate(self):
-        return [(self, [self.argument], [])]
+        if isinstance(self.argument, Forall):
+            expr = Exists(self.argument.left, Neg(self.argument.right))
+            return [(self, [], [expr])]
+        else:
+            return [(self, [self.argument], [])]
 
 
-class Implication(BinaryOp):
+class Imp(BinaryOp):
     def introduce(self):
         return [(self, [], [self.left]), (self, [self.right], [])]
 
@@ -106,7 +98,7 @@ class Implication(BinaryOp):
         return [(self, [self.left], [self.right])]
 
 
-class Disjunction(BinaryOp):
+class Disj(BinaryOp):
     def introduce(self):
         return [(self, [self.left], []), (self, [self.right], [])]
 
@@ -114,7 +106,7 @@ class Disjunction(BinaryOp):
         return [(self, [], [self.left, self.right])]
 
 
-class Conjunction(BinaryOp):
+class Conj(BinaryOp):
     def introduce(self):
         return [(self, [self.left, self.right], [])]
 
@@ -127,16 +119,9 @@ index = 0
 
 class Forall(BinaryOp):
     def __init__(self, left, right):
-        if isinstance(left, Term) and (left.args is None) and not isinstance(right, Term):
-            self.left = left
-            self.right = right
-            self.depth = 0
-        else:
-            if not(isinstance(left, Term) and (left.args is None)):
-                raise ValueError(f'In {self.__class__.__name__}, {left.show()} should be Var, not {left.__class__.__name__}.')
-            if isinstance(right, Term):
-                raise ValueError(f'In {self.__class__.__name__}, {right.show()} should be Expr, not {right.__class__.__name__}.')
-            
+        super().__init__(left, right)
+        self.depth = 0
+
     def __eq__(self, other):
         if self.__class__ == other.__class__:
             if self.left != other.left:
@@ -151,24 +136,21 @@ class Forall(BinaryOp):
         index += 1
         if self.depth < 1:
             self.depth += 1
-            return [(self, [Substitute(Term(f'_v{index}'), substitute(self.left, Term(f'_v{index}'), self.right)), self], [])]
+            return [
+                (self, [Substitute(Var(f'_v{index}'), substitute(self.left, Var(f'_v{index}'), self.right)), self], [])]
         else:
-            return [(self, [Substitute(Term(f'_v{index}'), substitute(self.left, Term(f'_v{index}'), self.right))], [])]
+            return [(self, [Substitute(Var(f'_v{index}'), substitute(self.left, Var(f'_v{index}'), self.right))], [])]
 
     def eliminate(self):
         global index
         index += 1
-        return [(self, [], [substitute(self.left, Term(f'_c{index}'), self.right)])]
+        return [(self, [], [substitute(self.left, Var(f'_c{index}'), self.right)])]
 
 
 class Exists(BinaryOp):
     def __init__(self, left, right):
-        if isinstance(left, Term) and (left.args is None) and not isinstance(right, Term):
-            self.left = left
-            self.right = right
-            self.depth = 0
-        else:
-            raise ValueError(f'In {self.__class__.__name__}, {left.show()} should be Var and {right.show()} should be Expr.')
+        super().__init__(left, right)
+        self.depth = 0
 
     def __eq__(self, other):
         if self.__class__ == other.__class__:
@@ -182,22 +164,22 @@ class Exists(BinaryOp):
     def introduce(self):
         global index
         index += 1
-        return [(self, [substitute(self.left, Term(f'_c{index}'), self.right)], [])]
+        return [(self, [substitute(self.left, Var(f'_c{index}'), self.right)], [])]
 
     def eliminate(self):
         global index
         index += 1
         if self.depth < 1:
             self.depth += 1
-            return [(self, [], [Substitute(Term(f'_v{index}'), substitute(self.left, Term(f'_v{index}'), self.right)), self])]
+            return [
+                (self, [], [Substitute(Var(f'_v{index}'), substitute(self.left, Var(f'_v{index}'), self.right)), self])]
         else:
-            return [(self, [], [Substitute(Term(f'_v{index}'), substitute(self.left, Term(f'_v{index}'), self.right))])]
+            return [(self, [], [Substitute(Var(f'_v{index}'), substitute(self.left, Var(f'_v{index}'), self.right))])]
 
 
 class Substitute(BinaryOp):
     def __init__(self, left, right):
-        self.left = left
-        self.right = right
+        super().__init__(left, right)
 
     def show(self):
         return f'{self.__class__.__name__}({self.left.show()} in {self.right.show()})'
@@ -209,29 +191,26 @@ class Substitute(BinaryOp):
             new_name = f'_v{index}'
         else:
             new_name = f'_c{index}'
-        self.right = substitute(self.left, Term(new_name), self.right)
-        self.left = Term(new_name)
+        self.right = substitute(self.left, Var(new_name), self.right)
+        self.left = Var(new_name)
 
 
 def substitute(old, new, expr):
     res = expr.copy()
     if expr == old:
         res = new
-    elif isinstance(expr, Atom):
-        for i in range(0, len(expr.args)):
-            res.args[i] = substitute(old, new, expr.args[i])
     elif issubclass(type(expr), UnaryOp):
         res.argument = substitute(old, new, res.argument)
-    elif isinstance(expr, Forall) or isinstance(expr, Exists) or isinstance(expr, Substitute):
+    elif isinstance(expr, Forall) or isinstance(expr, Exists):
         if expr.left != old:
             res.right = substitute(old, new, res.right)
         else:
             global index
             index += 1
             if expr.left.name.startswith('_v'):
-                res.left = Term(f'_v{index}')
+                res.left = Var(f'_v{index}')
             else:
-                res.left = Term(f'_c{index}')
+                res.left = Var(f'_c{index}')
             res.right = substitute(expr.left, res.left, res.right)
             res.right = substitute(old, new, res.right)
     elif issubclass(type(expr), BinaryOp):
